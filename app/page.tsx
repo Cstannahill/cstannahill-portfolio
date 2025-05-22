c5c0b4c3b426007c166e3146739878320ee9ff51
Cstannahill <cstannahill10@gmail.com>
2025-05-22 03:10:27 -0500

    fix on page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  // Redirect to the default locale (English)
+  // Redirect to the default locale (English) for now
   redirect("/en");
 }
